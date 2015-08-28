6f8071ff3b47e8c72005de147ef34547addfe843
sjt218 <sjt218@lehigh.edu>
2015-08-28 13:35:45 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,28 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Salvador Joshua Tarun III
+//Hello World Java Program
+//
+//Compile program first
+//Use javac HelloWorld.java in the command prompt
+//Run the program
+//Uses java HelloWorld
+
+//Define a class
+public class HelloWorld{
+    
+    // add main method
+    public static void main(String[] args) {
+        
+        //print the statement with the box around it
+        System.out.println("#######################");
+        System.out.println("#######################");
+        System.out.println("#                     #");
+        System.out.println("#  Hello World 2014!  #");
+        System.out.println("#                     #");
+        System.out.println("#######################");
+        System.out.println("#######################");
+        System.out.println("test");
+        
+        //test/testtest
+    }
+}
