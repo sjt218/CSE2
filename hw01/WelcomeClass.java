38d722677afe396457e9ccf7c69e5e7171cacdd3
sjt218 <sjt218@lehigh.edu>
2015-08-29 16:15:38 +0000

    updated Welcome Class

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,28 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Salvador Joshua Tarun III
+//Welcome Class Java Program
+//
+//Compile program first
+//Use javac WelcomeClass.java in the command prompt
+//Run the program
+//Uses java WelcomeClass
+
+//Define a class
+public class WelcomeClass{
+    
+    // add main method
+    public static void main(String[] args) {
+        
+        //print the statement with the box around it
+        System.out.println("     -----------    ");
+        System.out.println("     | WELCOME |    ");
+        System.out.println("     -----------    ");
+        System.out.println("  ^  ^  ^  ^  ^  ^  ");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("|-S--J--T--2--1--8-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  v  v  v  v  v  v  ");
+        System.out.println("I had an Electrical Engineer Internship this summer, and relaxed in Florida for a week before school. We kayaked and saw the manatees.");
+        
+    }
+}
